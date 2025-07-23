2a405928adb908e2b96157a14363b24d14b309a8
davidyow <36060726+davidyow@users.noreply.github.com>
2025-07-23 17:13:07 +1000

    Updates

--- courses-app/backend/index.js
+++ courses-app/backend/index.js
@@ -1,32 +1,87 @@
-const express = require('express');
-const cors = require('cors');
+import express, { json } from 'express';
+import cors from 'cors'; // Import the cors package
+import pkg from 'mongoose';
+const { connect, connection, Schema, model, Types } = pkg;
+import { v4 as uuidv4 } from 'uuid';
+import { config } from 'dotenv';
+config();
+
+//const express = require('express');
+//const cors = require('cors');
 
 const app = express();
-const port = 5999;
+const port = process.env.PORT || 5999;
+//const port = 5999;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// MongoDB connection
+connect(process.env.MONGODB_URI);
+
+const db = connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+});
+
+// Define Mongoose schemas and models
+const courseSchema = new Schema({
+    title: String,
+    description: String,
+    instructor: String,
+    duration: Number,
+    category: String
+});
+
+const Course = model('Course', courseSchema);
 
 // Define a simple route
 app.get('/', (req, res) => {
   res.send('Welcome to IATD 2024!');
 });
 
+// Retrieve all courses
+app.get('/courses', (req, res) => {
+    Course.find({})
+        .then(courses => {
+            res.json(courses);
+        })
+        .catch(error => {
+            console.error(error);
+            res.status(500).send('Error retrieving courses');
+        });
+});
+
+// Retrieve a single course by course_id
+app.get('/courses/:courseId', (req, res) => {
+    Course.findById(req.params.courseId)
+        .then(student => {
+            if (!course) {
+                return res.status(404).send('Course not found');
+            }
+            res.json(course);
+        })
+        .catch(error => {
+            console.error(error);
+            res.status(500).send('Error retrieving course');
+        });
+});
 // Array to store items
-let items = [];
+//let items = [];
 
 // Route to get all items
-app.get('/items', (req, res) => {
-  res.json(items);
-});
+//app.get('/items', (req, res) => {
+//  res.json(items);
+//});
 
 // Route to add a new item
-app.post('/items', (req, res) => {
-  const newItem = req.body.name;
-  items.push(newItem);
-  res.status(201).json({ name: newItem });
-});
+//app.post('/items', (req, res) => {
+//  const newItem = req.body.name;
+//  items.push(newItem);
+//  res.status(201).json({ name: newItem });
+//});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
